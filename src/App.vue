f71719aa161a17db4bd43e2431e6cfafbddee87c
csslick <digitone@naver.com>
2024-10-17 17:44:11 +0900

    프로젝트 셋업

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="container">
+    <h1>Vue3 Job</h1>
+    <p>text color</p>
+    <a href="#">link</a>
+  </div>
+</template>
+
+<style scoped lang="scss">
+  div {
+    h1 {
+      color: red;
+        &:hover {
+        color: blue;
+      }
+    }
+  }
+</style>
