f1f57654b2481ca1ad3b66577e8cdce9a90e4d0a
csslick <digitone@naver.com>
2024-10-26 00:03:23 +0900

    29.로그인 상태 확인 후 화면 보여주기

--- src/view/UserProfile.vue
+++ src/view/UserProfile.vue
@@ -1,13 +1,16 @@
 <template>
-  <h1>UserProfile</h1>
+  <div class="container" v-if="isLogin">
+    <h1>UserProfile</h1>
+  </div>
 </template>
   
 <script setup>
 import supabase from '../supabase';
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
 import { useRouter } from 'vue-router';
 
 const router = useRouter()
+const isLogin = ref(false); // 화면 표시 상태 변수
 
 // 마운트시 로그인 상태 확인하기
 onMounted(async() => {
@@ -16,8 +19,10 @@ onMounted(async() => {
 
   if(user) {
     console.log('로그인 상태')
+    isLogin.value = true
   } else {
     console.log('로그아웃 상태')
+    isLogin.value = false
     alert('로그인 후 이용해주세요.')
     router.push('/')
   }
