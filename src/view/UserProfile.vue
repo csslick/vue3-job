19ed8af80f66f99101e8a6098b8787caa7b85a77
csslick <digitone@naver.com>
2024-10-26 13:14:47 +0900

    30.로그아웃 기능 구현

--- src/view/UserProfile.vue
+++ src/view/UserProfile.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="container" v-if="isLogin">
-    <h1>UserProfile</h1>
+    <div class="form-container">
+      <button class="logout" @click="handleLogout">로그아웃</button>
+    </div>
   </div>
 </template>
   
@@ -11,6 +13,16 @@ import { useRouter } from 'vue-router';
 
 const router = useRouter()
 const isLogin = ref(false); // 화면 표시 상태 변수
+
+const handleLogout = async () => {
+  const { error } = await supabase.auth.signOut()
+  if(error) {
+    alert('로그아웃 실패')
+  } else {
+    alert('로그아웃 성공')
+    router.push('/')
+  }
+}
 
 // 마운트시 로그인 상태 확인하기
 onMounted(async() => {
@@ -31,6 +43,16 @@ onMounted(async() => {
   
 </script>
   
-<style>
-  
+<style lang="scss" scoped>
+  // 버튼 디자인 수정
+  button { 
+    background: transparent;
+    color: var(--main-color);
+    font-size: 16px;
+    margin-top: 40px;
+    &:hover{
+      opacity: 0.7;
+      text-decoration: underline;
+    }
+  }  
 </style>
